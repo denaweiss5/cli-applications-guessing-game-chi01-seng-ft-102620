e2a6519f2af52221297419a784922543ca9a9f85
Dena Neuman <denaweiss5@gmail.com>
2020-10-23 00:48:44 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -1,7 +1,7 @@
 def run_guessing_game
   puts "Guess a number between 1 and 6."
-  random_number= rand(6) + 1
-  user_input = gets.chomp.to_i
+  random_number= (rand(6) + 1).to_s
+  user_input = gets.chomp
 
   if user_input == random_number
     puts "You guessed the correct number!"
